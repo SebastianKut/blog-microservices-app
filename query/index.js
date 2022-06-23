714773eee615d6742bd7ad7c362410b4f601f693
sebastiankut <sebastian_kut@yahoo.co.uk>
2022-06-23 16:51:37 +0200

    added ingress

--- query/index.js
+++ query/index.js
@@ -45,7 +45,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('Listening on 4002');
   const res = await axios
-    .get('http://localhost:4005/events')
+    .get('http://event-bus-srv:4005/events')
     .catch((err) => console.log(err.message));
   for (let event of res.data) {
     console.log(`Processing event ${event.type}`);
